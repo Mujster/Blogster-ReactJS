85f53f7896b7be00e7eacd400138bf53409c6e19
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-02-25 00:36:09 +0500

    Blogster Frontend

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/Navbar.css';
+
+const Navbar=()=>{
+    return(
+        <div className="navbar">
+             <div>
+
+                <h1>Blogster</h1>
+             </div>
+        </div>
+    );
+};
+
+export default Navbar;
